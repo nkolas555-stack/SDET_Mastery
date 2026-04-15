76f7f2c627b989e57242f5e2094695523a8f116c
Lorenzo Nicolas de la Cruz <nkolas555@gmail.com>
2026-04-14 20:09:07 -0600

    Added Selenium and WebDriverManager dependencies - First functional browser test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,21 @@
-package org.example;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // 1. Configura el driver de Chrome automáticamente
+        WebDriverManager.chromedriver().setup();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // 2. Abre el navegador
+        WebDriver driver = new ChromeDriver();
+
+        // 3. Ve a tu LinkedIn para validar el éxito
+        driver.get("https://linkedin.com");
+
+        System.out.println("Page Title is: " + driver.getTitle());
+
+        // 4. Cierra el navegador
+        driver.quit();
     }
 }
